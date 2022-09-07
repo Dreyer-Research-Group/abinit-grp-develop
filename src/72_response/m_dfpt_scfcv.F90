55659a4696ee44b55dd88a7c9ce3cd1889151866
Cyrus Dreyer <cyrus.dreyer@stonybrook.edu>
2022-09-07 15:24:07 -0400

    Still trying to figure out sphere integration

--- src/72_response/m_dfpt_scfcv.F90
+++ src/72_response/m_dfpt_scfcv.F90
@@ -1048,21 +1048,11 @@ if (dtset%prtfomag > 0 .and. me==0) then
       write(*,'(a13,3i5,2e20.10e3)') 'MY_DEN_AT',ipert,idir,iatom,intgden_cplex(:,3,iatom)
       write(*,'(a13,3i5,2e20.10e3)') 'MZ_DEN_AT',ipert,idir,iatom,intgden_cplex(:,4,iatom)
       
-      open (unit=19, file='dmdtau_at.dat', status='old',access='append')
-      write(19,'(3i5,3f10.5,6e20.10e3)') ipert,idir,iatom,dtset%qptn(:), & 
-           & intgden_cplex(:,2,iatom),intgden_cplex(:,3,iatom),intgden_cplex(:,4,iatom)
-      close(19)
-
    end do
    write(*,'(a13,2i5,2e20.10e3)') 'N_DEN_TOT',ipert,idir,rhomag(:,1)
    write(*,'(a13,2i5,2e20.10e3)') 'MX_DEN_TOT',ipert,idir,rhomag(:,2)
    write(*,'(a13,2i5,2e20.10e3)') 'MY_DEN_TOT',ipert,idir,rhomag(:,3)
    write(*,'(a13,2i5,2e20.10e3)') 'MZ_DEN_TOT',ipert,idir,rhomag(:,4)
-
-   open (unit=19, file='dmdtau_tot.dat', status='old',access='append')
-      write(19,'(2i5,3f10.5,6e20.10e3)') ipert,idir,dtset%qptn(:),rhomag(:,2),rhomag(:,3),rhomag(:,4)
-   close(19)
-
 
 end if
 
@@ -1367,11 +1357,22 @@ end if
        write(*,'(a13,3i5,2e20.10e3)') 'MX_DEN_AT',ipert,idir,iatom,intgden_cplex(:,2,iatom)
        write(*,'(a13,3i5,2e20.10e3)') 'MY_DEN_AT',ipert,idir,iatom,intgden_cplex(:,3,iatom)
        write(*,'(a13,3i5,2e20.10e3)') 'MZ_DEN_AT',ipert,idir,iatom,intgden_cplex(:,4,iatom)
+
+       open (unit=19, file='dmdtau_at.dat', status='old',access='append')
+       write(19,'(3i5,3f10.5,6e20.10e3)') ipert,idir,iatom,dtset%qptn(:), & 
+            & intgden_cplex(:,2,iatom),intgden_cplex(:,3,iatom),intgden_cplex(:,4,iatom)
+       close(19)
+       
     end do
     write(*,'(a23,2i5,2e20.10e3)') 'FINAL_N_DEN_TOT',ipert,idir,rhomag(:,1)
     write(*,'(a23,2i5,2e20.10e3)') 'FINAL_MX_DEN_TOT',ipert,idir,rhomag(:,2)
     write(*,'(a23,2i5,2e20.10e3)') 'FINAL_MY_DEN_TOT',ipert,idir,rhomag(:,3)
     write(*,'(a23,2i5,2e20.10e3)') 'FINAL_MZ_DEN_TOT',ipert,idir,rhomag(:,4)
+
+    open (unit=19, file='dmdtau_tot.dat', status='old',access='append')
+    write(19,'(2i5,3f10.5,6e20.10e3)') ipert,idir,dtset%qptn(:),rhomag(:,2),rhomag(:,3),rhomag(:,4)
+    close(19)
+
 
     call  prtdenmagsph(cplex,intgden,dtset%natom,nspden,dtset%ntypat,ab_out,idir+1,dtset%ratsm,dtset%ratsph,rhomag,dtset%typat) 
  end if
